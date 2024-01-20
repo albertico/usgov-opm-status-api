86d5ffb372fe8ff1a6b7a24ab41ed59ef9e4dcad
Alberto Colón Viera <aacv@albertico.dev>
2024-01-20 16:13:07 -0500

    Updating tests: 'test_api.rb'

--- test/usgov/opm/status/test_api.rb
+++ test/usgov/opm/status/test_api.rb
@@ -6,8 +6,4 @@ class USGov::OPM::Status::TestAPI < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::USGov::OPM::Status::API::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
