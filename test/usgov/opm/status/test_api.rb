c30602a4bf9abd7bf8a482e935a261142d8d998a
Alberto Colón Viera <aacv@albertico.dev>
2024-01-27 23:06:41 -0500

    Updating tests in 'test_api.rb'

--- test/usgov/opm/status/test_api.rb
+++ test/usgov/opm/status/test_api.rb
@@ -3,7 +3,11 @@
 require "test_helper"
 
 class USGov::OPM::Status::TestAPI < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::USGov::OPM::Status::API::VERSION
+  def test_that_status_api_module_exist
+    assert(defined?(USGov::OPM::Status::API))
+  end
+
+  def test_that_status_api_has_version_number
+    refute_nil(USGov::OPM::Status::API::VERSION)
   end
 end
