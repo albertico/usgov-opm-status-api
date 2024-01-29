5da47f3091390997c4d071686ab253ee79af4b4e
Alberto Colón Viera <aacv@albertico.dev>
2024-01-29 18:51:44 -0500

    Requiring 'minitest/mock' in 'test_helper.rb'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,9 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "usgov/opm/status/api"
 
-require "webmock/minitest"
 require "minitest/autorun"
+require "minitest/mock"
+require "webmock/minitest"
 
 # Currently, the intent is to allow the test suite to reach the real API endpoints as a
 # a default,but make the HTTP library fail in certain tests to make sure that errors are
