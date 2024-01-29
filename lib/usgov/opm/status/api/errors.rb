d9c87eb5eec76b2a12bd41ee5c8d551feaf9eb88
Alberto Colón Viera <aacv@albertico.dev>
2024-01-29 00:17:59 -0500

    Refactoring error classes, adding the 'Errable' helper module and improving tests

--- lib/usgov/opm/status/api/errors.rb
+++ lib/usgov/opm/status/api/errors.rb
@@ -4,13 +4,94 @@ module USGov
   module OPM
     module Status
       module API
-        class HTTPError < StandardError
-        end
+        class Error < StandardError; end
 
-        class ClientError < HTTPError
-        end
+        class InvalidResponseFormatError < Error; end
 
-        class ServerError < HTTPError
+        class InvalidStatusTypeFormatError < InvalidResponseFormatError; end
+
+        class InvalidStatusTypeError < InvalidStatusTypeFormatError; end
+
+        class ResponseParsingError < Error; end
+
+        class HTTPError < Error; end
+
+        class ClientError < HTTPError; end
+        class ServerError < HTTPError; end
+
+        # 400 - Bad Request
+        class BadRequestError < ClientError; end
+        # 401 - Unauthorized
+        class UnauthorizedError < ClientError; end
+        # 403 - Forbidden
+        class ForbiddenError < ClientError; end
+        # 404 - Not Found
+        class NotFoundError < ClientError; end
+        # 422 - Unprocessable Entity
+        class UnprocessableEntityError < ClientError; end
+        # 429 - Too Many Requests
+        class TooManyRequestsError < ClientError; end
+
+        # 500 - Internal Server Error
+        class InternalServerError < ServerError; end
+
+        # USGov::OPM::Status::API::Errable
+        module Errable
+          HTTP_CLIENT_ERRORS = {
+            400 => BadRequestError,
+            401 => UnauthorizedError,
+            403 => ForbiddenError,
+            404 => NotFoundError,
+            422 => UnprocessableEntityError,
+            429 => TooManyRequestsError
+          }.freeze
+
+          HTTP_SERVER_ERRORS = {
+            500 => InternalServerError
+          }.freeze
+
+          protected
+
+          def raise_response_parsing_error(error_message)
+            raise(ResponseParsingError, "Error parsing HTTP response: #{error_message}")
+          end
+
+          def raise_invalid_response_format_error(error_message)
+            raise(InvalidResponseFormatError, "Invalid response format: #{error_message}")
+          end
+
+          def raise_unexpected_http_error(error_message)
+            raise(HTTPError, "Unexpected HTTP error: #{error_message}")
+          end
+
+          def raise_client_error(error_message, error = ClientError)
+            error = ClientError unless error.ancestors.include?(ClientError)
+            raise(error, "Client error: #{error_message}")
+          end
+
+          def raise_server_error(error_message, error = ServerError)
+            error = ServerError unless error.ancestors.include?(ServerError)
+            raise(error, "Server error: #{error_message}")
+          end
+
+          def raise_http_error_based_on_response_status_code(http_status_code) # rubocop:disable Metrics/MethodLength
+            # Raise generic HTTPError if status code is not an Integer.
+            raise_unexpected_http_error("Invalid status code") unless http_status_code.is_a?(Integer)
+
+            # Do nothing if status code is 200 OK.
+            return if http_status_code == 200
+
+            error_message = "#{http_status_code} #{HTTP::Response::Status::REASONS[http_status_code]}"
+
+            case http_status_code
+            when *HTTP_CLIENT_ERRORS.keys
+              raise_client_error(error_message, HTTP_CLIENT_ERRORS[http_status_code])
+            when *HTTP_SERVER_ERRORS.keys
+              raise_server_error(error_message, HTTP_SERVER_ERRORS[http_status_code])
+            else
+              raise_unexpected_http_error(error_message)
+            end
+          end
         end
       end
     end
--- test/usgov/opm/status/api/test_errors.rb
+++ test/usgov/opm/status/api/test_errors.rb
@@ -2,16 +2,147 @@
 
 require "test_helper"
 
-class USGov::OPM::Status::API::TestErrors < Minitest::Test
-  def test_that_http_error_class_exist
-    assert defined?(USGov::OPM::Status::API::HTTPError)
+class USGov::OPM::Status::API::TestErrors < Minitest::Test # rubocop:disable Metrics/ClassLength
+  def setup # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+    @error = USGov::OPM::Status::API::Error.new
+    @invalid_response_format_error = USGov::OPM::Status::API::InvalidResponseFormatError.new
+    @invalid_status_type_format_error = USGov::OPM::Status::API::InvalidStatusTypeFormatError.new
+    @invalid_status_type_error = USGov::OPM::Status::API::InvalidStatusTypeError.new
+    @response_parsing_error = USGov::OPM::Status::API::ResponseParsingError.new
+    @http_error = USGov::OPM::Status::API::HTTPError.new
+    @client_error = USGov::OPM::Status::API::ClientError.new
+    @server_error = USGov::OPM::Status::API::ServerError.new
+    @bad_request_error = USGov::OPM::Status::API::BadRequestError.new
+    @unauthorized_error = USGov::OPM::Status::API::UnauthorizedError.new
+    @forbidden_error = USGov::OPM::Status::API::ForbiddenError.new
+    @not_found_error = USGov::OPM::Status::API::NotFoundError.new
+    @unprocessable_entity_error = USGov::OPM::Status::API::UnprocessableEntityError.new
+    @too_many_requests_error = USGov::OPM::Status::API::TooManyRequestsError.new
+    @internal_server_error = USGov::OPM::Status::API::InternalServerError.new
+    @errable_obj = Object.new
+    @errable_obj.extend(USGov::OPM::Status::API::Errable)
   end
 
-  def test_that_client_error_class_exist
-    assert defined?(USGov::OPM::Status::API::ClientError)
+  def test_that_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::Error, @error)
+    assert_kind_of(StandardError, @error)
   end
 
-  def test_that_server_error_class_exist
-    assert defined?(USGov::OPM::Status::API::ServerError)
+  def test_that_invalid_response_format_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::InvalidResponseFormatError, @invalid_response_format_error)
+    assert_kind_of(USGov::OPM::Status::API::Error, @invalid_response_format_error)
+  end
+
+  def test_that_invalid_status_type_format_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::InvalidStatusTypeFormatError, @invalid_status_type_format_error)
+    assert_kind_of(USGov::OPM::Status::API::InvalidResponseFormatError, @invalid_status_type_format_error)
+  end
+
+  def test_that_invalid_status_type_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::InvalidStatusTypeError, @invalid_status_type_error)
+    assert_kind_of(USGov::OPM::Status::API::InvalidStatusTypeFormatError, @invalid_status_type_error)
+  end
+
+  def test_that_response_parsing_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::ResponseParsingError, @response_parsing_error)
+    assert_kind_of(USGov::OPM::Status::API::Error, @response_parsing_error)
+  end
+
+  def test_that_http_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::HTTPError, @http_error)
+    assert_kind_of(USGov::OPM::Status::API::Error, @http_error)
+  end
+
+  def test_that_client_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::ClientError, @client_error)
+    assert_kind_of(USGov::OPM::Status::API::HTTPError, @client_error)
+  end
+
+  def test_that_server_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::ServerError, @server_error)
+    assert_kind_of(USGov::OPM::Status::API::HTTPError, @server_error)
+  end
+
+  def test_that_bad_request_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::BadRequestError, @bad_request_error)
+    assert_kind_of(USGov::OPM::Status::API::ClientError, @bad_request_error)
+  end
+
+  def test_that_unauthorized_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::UnauthorizedError, @unauthorized_error)
+    assert_kind_of(USGov::OPM::Status::API::ClientError, @unauthorized_error)
+  end
+
+  def test_that_forbidden_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::ForbiddenError, @forbidden_error)
+    assert_kind_of(USGov::OPM::Status::API::ClientError, @forbidden_error)
+  end
+
+  def test_that_not_found_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::NotFoundError, @not_found_error)
+    assert_kind_of(USGov::OPM::Status::API::ClientError, @not_found_error)
+  end
+
+  def test_that_unprocessable_entity_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::UnprocessableEntityError, @unprocessable_entity_error)
+    assert_kind_of(USGov::OPM::Status::API::ClientError, @unprocessable_entity_error)
+  end
+
+  def test_that_too_many_requests_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::TooManyRequestsError, @too_many_requests_error)
+    assert_kind_of(USGov::OPM::Status::API::ClientError, @too_many_requests_error)
+  end
+
+  def test_that_internal_server_error_can_be_instantiated
+    assert_kind_of(USGov::OPM::Status::API::InternalServerError, @internal_server_error)
+    assert_kind_of(USGov::OPM::Status::API::ServerError, @internal_server_error)
+  end
+
+  def test_that_errable_raises_http_error_if_status_code_not_integer
+    assert_raises(USGov::OPM::Status::API::HTTPError) do
+      @errable_obj.send(:raise_http_error_based_on_response_status_code, nil)
+    end
+  end
+
+  def test_that_errable_raises_bad_request_error
+    assert_raises(USGov::OPM::Status::API::BadRequestError) do
+      @errable_obj.send(:raise_http_error_based_on_response_status_code, 400)
+    end
+  end
+
+  def test_that_errable_raises_unauthorized_error
+    assert_raises(USGov::OPM::Status::API::UnauthorizedError) do
+      @errable_obj.send(:raise_http_error_based_on_response_status_code, 401)
+    end
+  end
+
+  def test_that_errable_raises_forbidden_error
+    assert_raises(USGov::OPM::Status::API::ForbiddenError) do
+      @errable_obj.send(:raise_http_error_based_on_response_status_code, 403)
+    end
+  end
+
+  def test_that_errable_raises_not_found_error
+    assert_raises(USGov::OPM::Status::API::NotFoundError) do
+      @errable_obj.send(:raise_http_error_based_on_response_status_code, 404)
+    end
+  end
+
+  def test_that_errable_raises_unprocessable_entity_error
+    assert_raises(USGov::OPM::Status::API::UnprocessableEntityError) do
+      @errable_obj.send(:raise_http_error_based_on_response_status_code, 422)
+    end
+  end
+
+  def test_that_errable_raises_too_many_requests_error
+    assert_raises(USGov::OPM::Status::API::TooManyRequestsError) do
+      @errable_obj.send(:raise_http_error_based_on_response_status_code, 429)
+    end
+  end
+
+  def test_that_errable_raises_internal_server_error
+    assert_raises(USGov::OPM::Status::API::InternalServerError) do
+      @errable_obj.send(:raise_http_error_based_on_response_status_code, 500)
+    end
   end
 end
