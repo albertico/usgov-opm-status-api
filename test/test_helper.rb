1b44bcd44fa256b2d694e2836b0ef281c9118920
Alberto Colón Viera <aacv@albertico.dev>
2024-01-30 01:22:54 -0500

    Updating 'test_helper.rb' to use 'mocha' instead of 'minitest/mock'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,8 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "usgov/opm/status/api"
 
 require "minitest/autorun"
-require "minitest/mock"
+# require "minitest/mock"
+require "mocha/minitest"
 require "webmock/minitest"
 
 # Currently, the intent is to allow the test suite to reach the real API endpoints as a
