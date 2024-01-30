3660a62002f27afb0d65f8ad86be0cd69ede97c1
Alberto Colón Viera <aacv@albertico.dev>
2024-01-30 01:31:32 -0500

    Updating 'api.rb' file to load 'client.rb' and 'status_type.rb'

--- lib/usgov/opm/status/api.rb
+++ lib/usgov/opm/status/api.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "api/client"
 require_relative "api/errors"
+require_relative "api/status_type"
 require_relative "api/v1"
 require_relative "api/version"
-
-module USGov
-  module OPM
-    module Status
-      module API
-
-      end
-    end
-  end
-end
